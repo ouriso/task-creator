cf4d70b86f0161c1010ed1bea2220286bd7ea2a4
ErBlack <erblack@live.ru>
2021-06-20 20:21:11 +0300

    Day string format

--- src/plugins/plugin_filter_day.py
+++ src/plugins/plugin_filter_day.py
@@ -3,6 +3,33 @@ from typing import List
 
 day = datetime.date.today().strftime('%a')
 
+DAYS = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun'
+]
 
-def plugin_filter_day(days: List[str]) -> bool:
-    return day in days
+# Mon,Wed-Fri for ['Mon', 'Wed', 'Thu', 'Fri']
+def parse_days(days: str):
+    chunks = days.split(',')
+    days_list = []
+
+    for chunk in chunks:
+        if '-' in chunk:
+            start, end = chunk.split('-')
+
+            days_list += DAYS[DAYS.index(start): DAYS.index(end) + 1]
+        else:
+            days_list.append(chunk)
+
+    return days_list
+
+
+def plugin_filter_day(days: str):
+    days_list = parse_days(days)
+
+    return day in days_list
