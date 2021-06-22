8c32c05a7ea10e1656d89da43745c35e6912f7ee
ertema <ertema@yandex-team.ru>
2021-06-22 21:25:58 +0300

    Validate script

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from src.models.config import Config
 from src.todoist_task import commit, create_task
 
 
-def task(request=None, context=None):
+def task(request=None, context=None, dry_run=False):
     json = get_config()
 
     config = Config(**json)
 
     for task in config.tasks:
         if task.should_create():
-            create_task(**task.get_params())
+            params = task.get_params()
+            if dry_run:
+                print(params)
+            else:
+                create_task(**params)
 
-    commit()
+    if not dry_run:
+        commit()
 
 
 if __name__ == '__main__':
